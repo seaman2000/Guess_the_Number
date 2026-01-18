9f2bd75a7db1b5520e46abdf5c7fc05a18388e8a
Georgi Angelov <gtangelov76@gmail.com>
2026-01-18 21:51:50 +0200

    Update guess_the_number.py

--- guess_the_number.py
+++ guess_the_number.py
@@ -18,8 +18,10 @@ closest_number = None
 while True:
     is_invalid = False
     player_number = None
-    if counter == 0: #choose difficulty
+
+    if counter == 0:  # choose difficulty
         difficulty = input("Choose difficulty between Easy, Medium or Hard: ").strip().lower()
+
         if difficulty == "easy":
             max_number = 20
         elif difficulty == "medium":
@@ -29,9 +31,12 @@ while True:
         else:
             print("Invalid difficulty. Please, choose between Easy, Medium or Hard.")
             continue
+
         troll_counter = 0
         computer_number = random.randint(1, max_number)
+
     player_input = input(f"Guess the number (1-{max_number}): ").strip()
+
     if not player_input.isdigit():
         print("Invalid input. Try again...")
         is_invalid = True
@@ -40,14 +45,17 @@ while True:
         if not player_number in range(1, max_number + 1):
             print(f"Please type a number which is between (1-{max_number})")
             is_invalid = True
+
     if is_invalid:
         troll_counter += 1
         if troll_counter == 3:
             print("You're joking...")
             break
         continue
+
     counter += 1
     player_guesses.append(player_number)
+
     if player_number == computer_number:
         print("You guessed it!")
         is_guessed = True
@@ -56,34 +64,42 @@ while True:
         print("Too high!")
     else:
         print("Too low!")
+
     if counter >= 5:
         print("You don't have any more tries left.")
+
         for each_integer in player_guesses:
             difference = abs(each_integer - computer_number)
             if difference < min_size:
                 min_size = difference
                 closest_number = each_integer
+
         if min_size <= 5:
             print("You were so close though!")
             print(f"From your guessed numbers, the closest was {closest_number}, "
                   f"{min_size} numbers away from the target.")
         print("Do you want to play again?")
+
         answer = input("Y or N ").strip().upper()
+
         while answer != "Y" and answer != "N":
             answer = input("I said Y or N!!!").strip().upper()
+
         if answer == "Y":
             closest_number = None
             player_guesses = []
             counter = 0
             min_size = float('inf')
+
             if games_counter >= 3:
                 print("I am too tired for this, I need a break.")
                 break
-            else:
-                games_counter += 1
-                continue
+
+            games_counter += 1
+            continue
         else:
             break
+
 if not is_guessed:
     print(f"The number to guess was: {computer_number}")
 
